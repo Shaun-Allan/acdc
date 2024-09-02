7eaa2bb2bd906609927fe34e2972533742d5bcdc
Shaun-Allan <shaunallan0605@gmail.com>
2024-09-02 18:50:17 +0530

    Repo created

---
+++ src/pages/Error/Error.jsx
@@ -0,0 +1,7 @@
+function Error(){
+    return <div className="flex-col w-screen h-screen flex items-center justify-center text-4xl font-bold bg-white text-black gap-10">
+        404 | Error Page
+    </div>
+}
+
+export default Error;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,8 @@
+
+function Error(){
+    return <div className="flex-col w-screen h-screen flex items-center justify-center text-4xl font-bold bg-white text-black gap-10">
+        Home Page
+    </div>
+}
+
+export default Error;
