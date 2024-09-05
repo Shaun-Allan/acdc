d350b15c23973ea2705cef8bb51faa69887d6a82
Shaun-Allan <shaunallan0605@gmail.com>
2024-09-06 01:08:17 +0530

    home added

---
+++ src/events_data.js
@@ -0,0 +1,15 @@
+const events = [
+    {
+        date: "12 - Jan - 2024",
+        img: "/hackathon.jpg",
+    },
+    {
+        date: "12 - Jan - 2024",
+        img: "/hackathon.jpg",
+    },
+    {
+        date: "12 - Jan - 2024",
+        img: "/hackathon.jpg",
+    },
+]
+export default events;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,30 @@
 import Navelement from "../Navelement/Navelement";
-import React, { useState, useEffect, useRef } from 'react'; 
+import React, { useState, useEffect, useRef } from "react";
+import {
+  Button,
+  Dropdown,
+  DropdownMenu,
+  DropdownTrigger,
+  NextUIProvider,
+  DropdownItem,
+  Tooltip,
+} from "@nextui-org/react";
+import { FaChevronDown } from "react-icons/fa";
+import Graphs from "./Graphs";
+import Fine from "./Fine";
 
-function Error() {
-    const [menuOpen, setMenuOpen] = useState(false);
+function Home() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [navDark, setNavDark] = useState(true);
   return (
-    <div className='w-screen flex flex-col items-center p-0 overflow-x-hidden overflow-clip bg-white h-screen'>
-      <Navelement menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      Home Page
+    <div className="w-screen flex flex-col items-center p-0 overflow-x-hidden bg-black h-screen">
+      <Navelement menuOpen={menuOpen} setMenuOpen={setMenuOpen} navDark={navDark} setNavDark={setNavDark}/>
+      <div className="w-screen flex flex-col sm:flex-row items-center px-4 py-4 sm:px-8 py-4 overflow-x-hidden bg-black h-full sm:h-screen gap-4 sm:gap-0">
+        <Graphs />
+        <Fine />
+      </div>
     </div>
   );
 }
 
-export default Error;
+export default Home;
