a438005be7624701cf820f43415875529257985f
Shaun Allan H <130825237+Shaun-Allan@users.noreply.github.com>
2024-09-06 02:02:03 +0530

    Update events_data.js

--- src/events_data.js
+++ src/events_data.js
@@ -1,14 +1,14 @@
 const events = [
     {
-        date: "12 - Jan - 2024",
+        date: "06 - September - 2024",
         img: "/graph1.png",
     },
     {
-        date: "12 - Jan - 2024",
+        date: "05 - September - 2024",
         img: "/graph2.png",
     },
     {
-        date: "12 - Jan - 2024",
+        date: "04 - September - 2024",
         img: "/graph3.png",
     },
 ]
